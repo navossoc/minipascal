84fb8238dd33562cb42740562c8c3110e9b17396
Guilherme Moreira <guilherme@gmmoreira.com>
2012-11-10 00:35:49 +0000

    Modificado o código para gerar uma árvore sintática abstrata.

--- javacc/src/jamil/SimpleNode2.java
+++ javacc/src/jamil/SimpleNode2.java
@@ -2,6 +2,11 @@ package jamil;
 
 public class SimpleNode2 extends SimpleNode {
 
+	public SimpleNode2(String value) {
+		super(0);
+		this.value = value;
+	}
+
 	public SimpleNode2(int i) {
 		super(i);
 	}
@@ -12,16 +17,8 @@ public class SimpleNode2 extends SimpleNode {
 
 	@Override
 	public void dump(String prefix) {
-		System.out.print(toString(prefix));
-		if(value != null)
-		{
-			System.out.print(" (" + value + ")");
-		}
-		System.out.println();
-		
-		// TODO: melhorar esse acesso
-		//LMP.graph.addVertex(this);
-		
+		System.out.println(toString(prefix));
+
 		if (children != null) {
 			for (int i = 0; i < children.length; ++i) {
 				SimpleNode n = (SimpleNode) children[i];
@@ -32,4 +29,17 @@ public class SimpleNode2 extends SimpleNode {
 		}
 	}
 
+	@Override
+	public String toString() {
+		if (value == null) {
+			return super.toString();
+		} else {
+			return value.toString();
+		}
+	}
+
+	@Override
+	public String toString(String prefix) {
+		return prefix + this.toString();
+	}
 }
